07e5f9dff7bbe1af972e52b7f0185a2e29b56b41
Nikhil Shinday <Nikhil@wr-130-64-97-71.medford.tufts.edu>
2013-10-25 21:14:49 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tuftstextblast::Application.config.secret_key_base = 'ec6166805f0db0cda71f804353ff066fa89ef188124cf37a9e84186d5632f7d9c077f1a9cfd1fe20f9afeb5ef4f475363f29ffbdec4234094c1fe3cd09e59589'
