18464a61c993f02def036015793b20544a8692bc
Alison <alictai@gmail.com>
2013-10-25 22:12:20 -0400

    updated routes.rb with stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Tuftstextblast::Application.routes.draw do
+	resources :leads, path: ''
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
